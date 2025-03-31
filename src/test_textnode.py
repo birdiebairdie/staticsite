e34a1beb2c2aef40b4a6dc9d2317f9db0f5a470d
birdiebairdie <birdiebairdie@gmail.com>
2025-03-31 18:51:03 -0400

    added test to html, split nodes, and tests for both

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -28,6 +28,48 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, url="")
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)   
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("this is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+
+    def test_link(self):
+        node = TextNode("this link goes to boop dot com", TextType.LINK, url="boop.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "this link goes to boop dot com")
+        self.assertEqual(html_node.props, {"href": "boop.com"})
+
+    def test_image(self):
+        node = TextNode("picture of a bumblebee", TextType.IMAGE, url="bumblebeepictures.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "bumblebeepictures.com", "alt": "picture of a bumblebee"})
+
+    def test_split_nodes_bold(self):
+        nodes = [
+            TextNode("This is text with a `code block` word", TextType.TEXT),
+            TextNode("This is text with a **bold** word", TextType.TEXT),
+            TextNode("This is text with an _italic_ word", TextType.TEXT),
+        ]
+
+        code_nodes = split_nodes_delimiter([nodes[0]], "`", TextType.CODE)
+        self.assertEqual(code_nodes[0], TextNode("This is text with a ", TextType.TEXT))
+        self.assertEqual(code_nodes[1], TextNode("code block", TextType.CODE))
+        self.assertEqual(code_nodes[2], TextNode(" word", TextType.TEXT))
+
+        bold_nodes = split_nodes_delimiter([nodes[1]], "**", TextType.BOLD)
+        self.assertEqual(bold_nodes[0], TextNode("This is text with a ", TextType.TEXT))
+        self.assertEqual(bold_nodes[1], TextNode("bold", TextType.BOLD))
+        self.assertEqual(bold_nodes[2], TextNode(" word", TextType.TEXT))
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
   TEXT = "text"
@@ -22,5 +23,44 @@ class TextNode:
     )
   def __repr__(self):
     return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+  match text_node.text_type:
+    case TextType.TEXT:
+      return LeafNode(tag=None, value=text_node.text)
+    case TextType.BOLD:
+      return LeafNode(tag="b", value=text_node.text)
+    case TextType.ITALIC:
+      return LeafNode(tag="i", value=text_node.text)
+    case TextType.CODE:
+      return LeafNode(tag="code", value=text_node.text)
+    case TextType.LINK:
+      return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    case TextType.IMAGE:
+      return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+    case _:
+      raise Exception("not a valid text type")
     
+def split_nodes_delimiter(old_nodes, delimiter, new_text_type):
+  new_nodes = []
+  for node in old_nodes:
+    if node.text_type != TextType.TEXT:
+      new_nodes += node
+    elif node.text.count(delimiter) != 2:
+      raise Exception("invalid syntax")
+    else:
+      split_node = node.text.split(delimiter)
+      node_list = [
+        TextNode(split_node[0], TextType.TEXT),
+        TextNode(split_node[1], new_text_type),
+        TextNode(split_node[2], TextType.TEXT),
+        ]
+      new_nodes.extend(node_list)
+  return new_nodes
+
+
+
+
+
+
   
