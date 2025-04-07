30f56276387dd775fc70c1349323e7ff26b7f776
birdiebairdie <birdiebairdie@gmail.com>
2025-04-07 14:29:06 -0400

    added BlockType enum, block_to_block_type function, and tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,17 +1,53 @@
 import re
+from enum import Enum
+
+class BlockType(Enum):
+  PARAGRAPH = 'paragraph'
+  HEADING = 'heading'
+  CODE = 'code'
+  QUOTE = 'quote'
+  ULIST = 'unordered_list'
+  OLIST = 'ordered_list'
 
 def markdown_to_blocks(markdown):
   split_markdown = markdown.split("\n\n")
-  print(f"initial split: {split_markdown}")
   stripped_markdown = []
   for item in split_markdown:
     if item == "":
       continue
     stripped_markdown.append(item.strip())
-  print(f"after stripping: {stripped_markdown}")
   pattern = "(\s{2,})"
   final = []
   for item in stripped_markdown:
     final.append(re.sub(pattern, '\n', item))
-  print(f"final: {final}")
   return final 
+
+def block_to_block_type(markdown):
+  lines = markdown.split('\n')
+  if re.match('^(#{1,6}\s).', markdown):
+    return BlockType.HEADING
+  if len(lines) > 1 and re.match('^`{3}\n', markdown) and re.search('\n`{3}$', markdown):
+    return BlockType.CODE
+  if re.match('^>', markdown):
+    for line in lines:
+      if not re.match('^>', line):
+        return BlockType.PARAGRAPH
+    return BlockType.QUOTE
+  if re.match('^(- )', markdown):
+    for line in lines:
+      if line == "":
+        continue
+      if not re.match ('^(- )', line):
+        return BlockType.PARAGRAPH
+    return BlockType.ULIST
+  if re.match('1. ', markdown):
+    i = 1
+    for line in lines:
+      if line == "":
+        continue
+      if not re.match (f'^{i}\. ', line):
+        return BlockType.PARAGRAPH
+      i += 1
+    return BlockType.OLIST
+  else:
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -2,22 +2,20 @@ import unittest
 
 from block_markdown import (
   markdown_to_blocks,
+  block_to_block_type, 
+  BlockType
 )
 
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
-        print("test markdown_to_blocks running")
-        md = """
-    This is **bolded** paragraph
+        md = """This is **bolded** paragraph
 
     This is another paragraph with _italic_ text and `code` here
     This is the same paragraph on a new line
 
     - This is a list
-    - with items
-    """
-        print(f"md: {md}")
+    - with items"""
 
         blocks = markdown_to_blocks(md)
         self.assertEqual(
@@ -28,6 +26,58 @@ class TestBlockMarkdown(unittest.TestCase):
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type(self):
+        md_heading = "### This is a heading"
+
+        md_code = """```
+This is a code block
+with several lines
+```
+"""
+        
+        md_quote = """>This is a properly
+>formatted
+>block quote."""
+
+        md_ulist = """- This is a properly
+- formatted
+- unordered
+- list.
+"""
+
+        md_ulist_wrong = """- This is an
+-improperly
+- formatted
+-unordered list.
+"""
+
+        md_olist = """1. This is a 
+2. properly formatted
+3. ordered list
+"""
+
+        md_paragraph = """
+This is a standard paragraph
+with multiple lines
+separated by line breaks
+"""
+
+        heading_type = block_to_block_type(md_heading)
+        code_type = block_to_block_type(md_code)
+        quote_type = block_to_block_type(md_quote)
+        ulist_type = block_to_block_type(md_ulist)
+        ulist_wrong_type = block_to_block_type(md_ulist_wrong)
+        olist_type = block_to_block_type(md_olist)
+        paragraph_type = block_to_block_type(md_paragraph)
+
+        self.assertEqual(heading_type, BlockType.HEADING)
+        self.assertEqual(code_type, BlockType.CODE)
+        self.assertEqual(quote_type, BlockType.QUOTE)
+        self.assertEqual(ulist_type, BlockType.ULIST)
+        self.assertEqual(ulist_wrong_type, BlockType.PARAGRAPH)
+        self.assertEqual(olist_type, BlockType.OLIST)
+        self.assertEqual(paragraph_type, BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
