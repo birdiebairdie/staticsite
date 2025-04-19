b9632ed8d6f3e9c0e50069448ad413e8d18a0312
birdiebairdie <birdiebairdie@gmail.com>
2025-04-19 14:25:26 -0400

    added function to clear public dir and remake with contents of static dir

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
-print("hello world")
+def copy_static_files(source, destination):
+  if os.path.exists(destination):
+    shutil.rmtree(destination)
+
+  os.mkdir(destination)
+
+  items = os.listdir(source)
+
+  for item in items:
+    source_item = os.path.join(source, item)
+    destination_item = os.path.join(destination, item)
+
+    if os.path.isfile(source_item):
+      shutil.copy(source_item, destination_item)
+      print(f"Copied file: {source_item} to {destination_item}")
+    else:
+      os.mkdir(destination_item)
+      print(f"Created directory: {destination_item}")
+      copy_static_files(source_item, destination_item)
+
 def main():
-  dummy_node = TextNode("text go here", TextType.LINK, "url go here")
-  print(dummy_node)
+  copy_static_files('static', 'public')
 
-main()
+if __name__ == "__main__":
+  main()
